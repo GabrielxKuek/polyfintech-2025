a99a32a4d676fa8a354660850307677e69d1d934
GabrielxKuek <kuekguanjiegabriel@gmail.com>
2025-06-26 16:15:51 +0800

    asdfghjk

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const viewsDir = path.join(__dirname, 'views')
+app.use(express.static(viewsDir))
+app.use(express.static(path.join(__dirname, './public')))
+app.use(express.static(path.join(__dirname, './weights')))
+app.use(express.static(path.join(__dirname, './dist')))
+
+app.get('/', (req, res) => res.redirect('/webcam_face_detection'))
+
+app.get('/webcam_face_detection', (req, res) => res.sendFile(path.join(viewsDir, 'webcamFaceDetection.html')))
+
+app.listen(3000, () => console.log('Listening on port 3000'))
